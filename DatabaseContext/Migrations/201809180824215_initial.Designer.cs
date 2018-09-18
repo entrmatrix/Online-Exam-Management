89ab5131fc0584c73168d38549bbdf5779f9f0f3
tafarabi <taafarabi@gmail.com>
2018-09-18 14:57:36 +0600

    Updated

--- DatabaseContext/Migrations/201809180824215_initial.Designer.cs
+++ DatabaseContext/Migrations/201809180824215_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace DatabaseContext.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Initial : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201809161111540_Initial"; }
+            get { return "201809180824215_initial"; }
         }
         
         string IMigrationMetadata.Source
--- Repository/OrganizationRepository.cs
+++ Repository/OrganizationRepository.cs
@@ -14,7 +14,7 @@ namespace Repository
         public bool Add(Organization organization)
         {
             db.Organizations.Add(organization);
-         return   db.SaveChanges() > 0;
+         return db.SaveChanges() > 0;
         }
 
         public List<Organization> GetAll()
