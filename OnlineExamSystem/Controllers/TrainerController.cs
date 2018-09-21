0f7a84e0f676c07358d1e628f9d3ad34a091ddb2
sadeknurul <sadeknurul5@gmail.com>
2018-09-21 16:39:28 +0600

    create exam and assign trainer

---
+++ OnlineExamSystem/Controllers/TrainerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExamSystem.Controllers
+{
+    public class TrainerController : Controller
+    {
+        //
+        // GET: /Trainer/
+        public ActionResult AssignTrainer()
+        {
+            return View();
+        }
+	}
+}
